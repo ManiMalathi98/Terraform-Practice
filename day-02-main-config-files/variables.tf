4ae98a1f011aaa3b152ec948dfc74cb6e112f83d
ManiMalathi98 <manimalathik1998@gmail.com>
2025-08-05 21:55:15 +0530

    day-02-main-config-files

---
+++ day-02-main-config-files/variables.tf
@@ -0,0 +1,12 @@
+
+variable "ami-id" {
+    description = "inerting the ami values to main.tf"
+    type = string
+    default = ""
+  
+}
+variable "instance-type" {
+    type = string
+    default = ""
+  
+}
