65b16d0c8cd11e1751431bd7d6451f3a2a492bb0
ManiMalathi98 <manimalathik1998@gmail.com>
2025-08-10 15:42:09 +0530

    day-4-state-locking

---
+++ day-4-statelocking/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "name" {
+    ami = "ami-08a6efd148b1f7504"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "dev-1"
+    }
+  
+}
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "devnew"
+    }
+  
+}
