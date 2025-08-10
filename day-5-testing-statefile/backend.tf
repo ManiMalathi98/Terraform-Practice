a76bc71637a444f2eb01cb13452b17c8178ba22e
ManiMalathi98 <manimalathik1998@gmail.com>
2025-08-10 18:27:31 +0530

    day-4-statelocking

--- day-5-testing-statefile/backend.tf
+++ day-5-testing-statefile/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "awsdevopsmani"
-    key    = "day-4/terraform.tfstate"
+    key    = "day-5/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
     #dynamodb_table = "mani"  #any version 
