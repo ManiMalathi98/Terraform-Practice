bcef4ac09c3d82229000f75e2e263c275c4e4e60
ManiMalathi98 <manimalathik1998@gmail.com>
2025-08-10 18:52:49 +0530

    day-5-testing-statefile

---
+++ day-5-testing-statefile/backend.tf
@@ -0,0 +1,11 @@
+
+terraform {
+  backend "s3" {
+    bucket = "awsdevopsmani"
+    key    = "day-5/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+    #dynamodb_table = "mani"  #any version 
+    #encrypt = true
+  }
+}
